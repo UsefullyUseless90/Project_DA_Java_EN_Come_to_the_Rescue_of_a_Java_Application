180150062f2d8baaa61774da731db9d840396173
UsefullyUseless90 <84840316+UsefullyUseless90@users.noreply.github.com>
2021-08-18 18:52:38 +0200

    Add files via upload

--- AnalyticsCounter.java
+++ AnalyticsCounter.java
@@ -1,5 +1,8 @@
 package com.hemebiotech.analytics;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -61,5 +64,27 @@ public class AnalyticsCounter {
 		}
 
 		return orderSymptoms;
+	}
+
+	public Map<String, Long> writeSymptoms(Map<String, Long> symptomsMapSorted) throws IOException {
+		String symptoms2 = "result" + String.valueOf("Out.txt");
+		FileWriter fstream = new FileWriter(symptoms2);
+		BufferedWriter out = new BufferedWriter(fstream);
+
+		for (Map.Entry<String, Long> entry : sortingSymptoms(symptomsMapSorted).entrySet()) {
+			System.out.println(entry.getKey() + " = " + entry.getValue()); // that's the printed result by the App//
+
+			try {
+				out.write("\n" + entry.getKey() + " = " + entry.getValue()); // that organize the order in the
+																				// file.txt//
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			out.flush();
+		}
+
+		out.close();
+		return symptomsMapSorted;
 	}
 }
